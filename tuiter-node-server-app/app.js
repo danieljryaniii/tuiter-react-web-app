bd08ea167944acf99ce80a98ac147e2fa3ff76fb
danieljryaniii <danieljryaniii@gmail.com>
2023-04-02 10:32:17 -0400

    Added tuits controller and functionality

--- tuiter-node-server-app/app.js
+++ tuiter-node-server-app/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(express.json());
 console.log("Server Started.............");
 console.log("Listening on Port 4000.....");
 
+TuitsController(app);
 HelloController(app);
 UsersController(app);
 app.listen(4000);
