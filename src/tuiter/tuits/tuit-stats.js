21e498d53ac56b5d21db5f6ef0f20eed921b6e9b
danieljryaniii <danieljryaniii@gmail.com>
2023-04-02 16:34:47 -0400

    Updated for dislike button

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -9,7 +9,17 @@ const TuitStats = ({item}) => {
             {...tuit,
                 likes: tuit.liked ? tuit.likes - 1 : tuit.likes + 1,
                 liked: !tuit.liked
-            }));
+            }
+        ));
+    };
+
+    const handleDislikeToggle = (tuit) => {
+        dispatch(updateTuitThunk(
+            {...tuit,
+                dislikes: tuit.disliked ? tuit.dislikes - 1 : tuit.dislikes + 1,
+                disliked: !tuit.disliked
+            }
+        ));
     };
 
     return (
@@ -25,6 +35,10 @@ const TuitStats = ({item}) => {
             <button onClick={() => handleLikeToggle(item)} className="text-secondary bg-white border-0">
                 {item.liked ? <i className="fa-solid fa-heart text-danger"></i> : <i className="fa-regular fa-heart"></i>}
                 <span className="ms-2">{item.likes}</span>
+            </button>
+            <button onClick={() => handleDislikeToggle(item)} className="text-secondary bg-white border-0">
+                {item.disliked ? <i className="fa-solid fa-thumbs-down text-warning"></i> : <i className="fa-regular fa-thumbs-down"></i>}
+                <span className="ms-2">{item.dislikes}</span>
             </button>
             <a className="text-secondary text-decoration-none" href="#">
                 <i className="fa-solid fa-arrow-up-right-from-square"></i>
